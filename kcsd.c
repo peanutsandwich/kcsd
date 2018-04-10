0a13b5ff64e1742699ce03f746fff54e9bd9faf9
Mark Walton <mark.walton@serialtek.com>
2018-04-10 14:37:08 +0100

    Converted license to Apache 2.0

--- kcsd.c
+++ kcsd.c
@@ -11,11 +11,19 @@
  * time. Messages that are received out of order (i.e. requests while awaiting a
  * resp, and resps while awaiting a request) are discarded.
  *
- * Copyright (C) 2018 SerialTek UK Ltd. - All Rights Reserved
- *
- * Unauthorized copying of this file, via any meium is strictly prohibited
- *
- * Proprietary and confidential
+ * Copyright (C) 2018 SerialTek UK Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 /*******************************************************************************
  * Includes
